6b5ce32d0ad6bf64ddde63a5e8f8d1f865cacbe6
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2024-08-06 16:20:51 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,16 @@
-import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
-import { Section } from './Section/Section';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+import { FriendList } from './FriendList/FriendList';
+import { Profile } from './Profile/Profile';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
     <>
-      <Section
-        color
-        children={
-          <Profile
-            username={user.username}
-            tag={user.tag}
-            location={user.location}
-            avatar={user.avatar}
-            stats={user.stats}
-          />
-        }
-      />
-      <Section
-        children={
-          <Profile
-            username={user.username}
-            tag={user.tag}
-            location={user.location}
-            avatar={user.avatar}
-            stats={user.stats}
-          />
-        }
-      />
-      <Section
-        color
-        children={
-          <Profile
-            username={user.username}
-            tag={user.tag}
-            location={user.location}
-            avatar={user.avatar}
-            stats={user.stats}
-          />
-        }
-      />
+      <Profile user={user} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
